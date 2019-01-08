dace0426b3ecb45b790e7e77a7ecb925ccec814c
vidhurraj147 <vidhurraj147@gmail.com>
2019-01-08 02:35:58 -0500

    creating a basic request.get which calls API

--- src/GithubApiService.ts
+++ src/GithubApiService.ts
@@ -1,9 +1,13 @@
-class GithubApiService{
-    getUserInfo(){
+import * as request from 'request';
 
+class GithubApiService{
+    getUserInfo(userName: string){
+        request.get('https://api.github.com/users/'+userName, null, (response: any) => {
+            console.log(response);
+        })
     }
 
     getRepos(){
-        
+
     }
 }
